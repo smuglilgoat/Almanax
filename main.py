661cd811ce4003228e1b853a44f8f463973dfed7
Ahmed Moussaoui <k.mame2810@gmail.com>
2021-01-26 20:41:04 +0000

    finished day cmd

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import discord
+from discord.ext import commands
 import os
+
 import datetime
 import json
 
-client = discord.Client()
+client = commands.Bot(command_prefix = '$')
 with open('almanax.json') as f:
   almanax = json.load(f)
 
@@ -11,20 +13,27 @@ with open('almanax.json') as f:
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@client.command()
+async def ping(ctx):
+  await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
 
-    if message.content.startswith('$day'):
-        now = datetime.datetime.now()
-        today_almanax = almanax['{:02d}/{:02d}'.format(now.day, now.month)]
-        await message.channel.send(today_almanax)
-    
-    if message.content.startswith('$week'):
-        await message.channel.send('ble39el calmi rohek wach :peepoWTF:!')
-    
-    if message.content.startswith('$month'):
-        await message.channel.send('ble39el calmi rohek wach :peepoWTF:!')
+@client.command()
+async def day(ctx):
+  now = datetime.datetime.now()
+  today = '{}-{:02d}-{:02d}'.format(now.year, now.month, now.day)
+  today_almanax = almanax['{:02d}/{:02d}'.format(now.day, now.month)]
+  image_id = today_almanax['itemImage']
+  embed=discord.Embed(
+    title=today_almanax["type"], 
+    url=f'http://www.krosmoz.com/fr/almanax/{today}', 
+    description=today_almanax["effect"],
+    color=0xe4bf07)
+  embed.set_author(name=today_almanax["quest"])
+  embed.set_thumbnail(url=f'http://static.ankama.com/dofus/www/game/items/200/{image_id}.png')
+  embed.add_field(
+    name=today_almanax['offering'], 
+    value=now, 
+    inline=False)
+  await ctx.send(embed=embed)
 
 client.run(os.getenv('TOKEN'))
